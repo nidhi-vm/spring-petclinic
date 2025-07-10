c10d94ead8aa65d644fcd9b62d27375753ac7693
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-07-10 15:55:50 -0400

    Updating using GitHub API 2025-07-10-15:55

--- src/test/java/org/springframework/samples/petclinic/system/CrashControllerIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/system/CrashControllerIntegrationTests.java
@@ -62,10 +62,12 @@ class CrashControllerIntegrationTests {
 	@Autowired
 	private TestRestTemplate rest;
 
+	private static final String OUPS_PATH = "/oups";
+
 	@Test
 	void testTriggerExceptionJson() {
 		ResponseEntity<Map<String, Object>> resp = rest.exchange(
-				RequestEntity.get("http://localhost:" + port + "/oups").build(),
+				RequestEntity.get("http://localhost:" + port + OUPS_PATH).build(),
 				new ParameterizedTypeReference<Map<String, Object>>() {
 				});
 		assertThat(resp).isNotNull();
@@ -75,14 +77,14 @@ class CrashControllerIntegrationTests {
 		assertThat(resp.getBody()).containsKey("error");
 		assertThat(resp.getBody()).containsEntry("message",
 				"Expected: controller used to showcase what happens when an exception is thrown");
-		assertThat(resp.getBody()).containsEntry("path", "/oups");
+		assertThat(resp.getBody()).containsEntry("path", OUPS_PATH);
 	}
 
 	@Test
 	void testTriggerExceptionHtml() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(List.of(MediaType.TEXT_HTML));
-		ResponseEntity<String> resp = rest.exchange("http://localhost:" + port + "/oups", HttpMethod.GET,
+		ResponseEntity<String> resp = rest.exchange("http://localhost:" + port + OUPS_PATH, HttpMethod.GET,
 				new HttpEntity<>(headers), String.class);
 		assertThat(resp).isNotNull();
 		assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
