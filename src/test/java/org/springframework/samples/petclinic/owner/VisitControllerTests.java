294098e9bf8af69776517f463475d84e86e044cb
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-07-10 15:55:49 -0400

    Updating using GitHub API 2025-07-10-15:55

--- src/test/java/org/springframework/samples/petclinic/owner/VisitControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/VisitControllerTests.java
@@ -37,8 +37,8 @@ import java.util.Optional;
 /**
  * Test class for {@link VisitController}
  *
- * @author Colin But
- * @author Wick Dynex
+ * author Colin But
+ * author Wick Dynex
  */
 @WebMvcTest(VisitController.class)
 @DisabledInNativeImage
@@ -48,6 +48,7 @@ class VisitControllerTests {
 	private static final int TEST_OWNER_ID = 1;
 
 	private static final int TEST_PET_ID = 1;
+	private static final String NEW_VISIT_URL = "/owners/{ownerId}/pets/{petId}/visits/new";
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -66,7 +67,7 @@ class VisitControllerTests {
 
 	@Test
 	void testInitNewVisitForm() throws Exception {
-		mockMvc.perform(get("/owners/{ownerId}/pets/{petId}/visits/new", TEST_OWNER_ID, TEST_PET_ID))
+		mockMvc.perform(get(NEW_VISIT_URL, TEST_OWNER_ID, TEST_PET_ID))
 			.andExpect(status().isOk())
 			.andExpect(view().name("pets/createOrUpdateVisitForm"));
 	}
@@ -74,7 +75,7 @@ class VisitControllerTests {
 	@Test
 	void testProcessNewVisitFormSuccess() throws Exception {
 		mockMvc
-			.perform(post("/owners/{ownerId}/pets/{petId}/visits/new", TEST_OWNER_ID, TEST_PET_ID)
+			.perform(post(NEW_VISIT_URL, TEST_OWNER_ID, TEST_PET_ID)
 				.param("name", "George")
 				.param("description", "Visit Description"))
 			.andExpect(status().is3xxRedirection())
@@ -84,7 +85,7 @@ class VisitControllerTests {
 	@Test
 	void testProcessNewVisitFormHasErrors() throws Exception {
 		mockMvc
-			.perform(post("/owners/{ownerId}/pets/{petId}/visits/new", TEST_OWNER_ID, TEST_PET_ID).param("name",
+			.perform(post(NEW_VISIT_URL, TEST_OWNER_ID, TEST_PET_ID).param("name",
 					"George"))
 			.andExpect(model().attributeHasErrors("visit"))
 			.andExpect(status().isOk())
